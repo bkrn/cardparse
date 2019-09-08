bb8d3d2fbae90b8c2a1cf10eb8f415094030c8a4
Aaron Di Silvestro <aaron@bkrn.org>
2019-09-07 17:01:05 -0700

    rerun build.rs if changed README in order to keep skeptic building in travis CI

--- build.rs
+++ build.rs
@@ -3,6 +3,9 @@
 extern crate skeptic;
 
 fn main() {
+    println!("rerun-if-changed=README.md");
+    println!("rerun-if-changed=src/lib.rs");
+    println!("rerun-if-changed=cardparse_derive/src/lib.rs");
     // generates doc tests for `README.md`.
     skeptic::generate_doc_tests(&["README.md"]);
 }
